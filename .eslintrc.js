57cb954fdf8c3e6f9187cac1dd092f23474fb345
Alexandre <alexandre.monteiro.bec@gmail.com>
2020-10-21 21:42:51 -0300

    chore: add gulp sass config and header page

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'comma-spacing': 2,
+    'no-multiple-empty-lines': [2, { max: 1 }],
+    'no-trailing-spaces': 2,
+  },
+};
---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const order = require("gulp-order");
+const concat = require("gulp-concat");
+
+const sassFolderPath = './scss';
+const sassFilesPath = './scss/*.scss';
+const sassBundleFileName = 'bundle.scss';
+
+const cssFolderDevelopment = './css';
+const cssFolderProduction = './build/css';
+
+const sassOptions = {
+  errLogToConsole: true,
+  outputStyle: 'expanded'
+};
+
+gulp.task('concat-sass', function () {
+  return gulp      
+    .src(sassFilesPath)
+    .pipe(order([
+      'scss/reset.scss',
+      'scss/variables.scss',
+      'scss/animations.scss',
+      'scss/header.scss',
+    ], { base: __dirname }))
+    .pipe(concat(sassBundleFileName))
+    .pipe(gulp.dest(sassFolderPath));
+});
+
+gulp.task('compile-sass-development', function () {
+  return gulp      
+    .src(`${sassFolderPath}/${sassBundleFileName}`)
+    .pipe(sass(sassOptions).on('error', sass.logError))    
+    .pipe(autoprefixer())
+    .pipe(gulp.dest(cssFolderDevelopment));
+});
+
+gulp.task('compile-sass-production', function () {
+  return gulp
+    .src(`${sassFolderPath}/${sassBundleFileName}`)
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest(cssFolderProduction));
+});
+
+gulp.task('development', gulp.series('concat-sass', 'compile-sass-development'));
+
+gulp.task('build', gulp.series('concat-sass', 'compile-sass-production'));
